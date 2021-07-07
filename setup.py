38c471b7ed3acc21ef0376999bbc3875a40f7963
Christophe Painchaud <shellescape@gmail.com>
2021-07-07 14:47:13 +0200

    initial support for packaging
    dgerg

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
 from setuptools import setup, find_packages
 
+#
+# with open('README.rst') as f:
+#     readme = f.read()
+#
+# with open('LICENSE') as f:
+#     license = f.read()
 
-with open('README.rst') as f:
-    readme = f.read()
-
-with open('LICENSE') as f:
-    license = f.read()
+readme = "TODO"
+licence = "TODO"
 
 setup(
     name='pylo',
-    version='0.1.0',
-    description='API Framework and Helpers for Illumio ASP platform',
+    version='0.2.0',
+    description='API Framework and Utilities for Illumio ASP platform',
     long_description=readme,
     author='Christophe Painchaud',
-    author_email='christophe.painchaud@illumio.com',
+    author_email='shellescape@gmail.com',
     url='',
     license=license,
     packages=find_packages(exclude=('tests', 'docs', 'pylo/vendors'))
