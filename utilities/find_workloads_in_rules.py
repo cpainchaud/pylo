d9a28002204b888c9d46f892b9107a7e00d3f281
Christophe Painchaud <shellescape@gmail.com>
2021-07-12 15:00:15 +0200

    fixes for find_workloads_in_rules utility

--- utilities/find_workloads_in_rules.py
+++ utilities/find_workloads_in_rules.py
@@ -9,29 +9,42 @@ import argparse
 
 parser = argparse.ArgumentParser(description='TODO LATER')
 parser.add_argument('--pce', type=str, required=True, help='hostname of the PCE')
-parser.add_argument('--only-deleted', type=bool, required=False, help='hostname of the PCE')
+parser.add_argument('--only-deleted', type=bool, required=False, nargs='?', const=True, help='only look for deleted workloads')
+parser.add_argument('--use-cache', type=bool, nargs='?', required=False, default=False, const=True,
+                    help='For developers only')
 
 args = vars(parser.parse_args())
 
 hostname = args['pce']
-
+settings_only_delete_workloads = args['only_deleted']
+settings_use_cache = args['use_cache']
 
 org = pylo.Organization(1)
 
-print("Loading Origin PCE configuration from " + hostname + " or cached file... ", end="", flush=True)
-org.load_from_cache_or_saved_credentials(hostname)
+print(" * Loading Origin PCE configuration from " + hostname + " or cached file... ", end="", flush=True)
+if settings_use_cache:
+    org.load_from_cache_or_saved_credentials(hostname)
+else:
+    org.load_from_saved_credentials(hostname)
 print("OK!\n")
 
+print(" * PCE statistics: ")
+print(org.stats_to_str(padding='    '))
+
+print()
+print("**** Now parsing workloads and rules.... ****")
+
 workloads_to_inspect = org.WorkloadStore.itemsByHRef.values()
-
-
 global_count_concerned_workloads = 0
-
 global_concerned_rulesets = {}
 global_concerned_rules = {}
 
 
 for workload in workloads_to_inspect:
+    if settings_only_delete_workloads:
+        if workload.temporary is True or workload.deleted is True:
+            continue
+
     concerned_rulesets: Dict[pylo.Ruleset, Dict[pylo.Rule,pylo.Rule]] = {}
     count_concerned_rules = 0
     for referencer in workload.get_references():
@@ -40,7 +53,7 @@ for workload in workloads_to_inspect:
             concerned_ruleset = concerned_rule.owner
 
             global_concerned_rulesets[concerned_ruleset] = True
-            global_concerned_rules[concerned_rule]= True
+            global_concerned_rules[concerned_rule] = True
 
             if concerned_ruleset not in concerned_rulesets:
                 concerned_rulesets[concerned_ruleset] = {concerned_rule: concerned_rule}
@@ -61,19 +74,18 @@ for workload in workloads_to_inspect:
 def get_name(obj):
     return obj.name
 
-
 tmp_rulesets = sorted(global_concerned_rulesets.keys(), key=get_name)
-print(tmp_rulesets)
 
-print("\n* For convenience here is the list of Rulesets:")
+
+print("\n* For convenience here is the consolidated list of Rulesets:")
 for ruleset in tmp_rulesets:
     print("  - '{}' HREF: {}".format(ruleset.name, ruleset.href))
 
 
-
-print("\n** Total: {} Workloads used in {} Rulesets and {} Rules".format(   global_count_concerned_workloads,
+print("\n*****  DONE with workloads & rules parsing  *****")
+print("** Total: {} Workloads used in {} Rulesets and {} Rules".format(   global_count_concerned_workloads,
                                                                             len(global_concerned_rulesets),
                                                                             len(global_concerned_rules)))
 
-print("\nEND OF SCRIPT\n")
+print("\n**** END OF SCRIPT ****\n")
 
