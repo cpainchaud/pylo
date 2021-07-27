fe61ea413e9aa216a218149281c03a06dfb333c7
Christophe Painchaud <shellescape@gmail.com>
2021-07-27 10:12:38 +0200

    fixed path

--- pylo/utilities/cli.py
+++ pylo/utilities/cli.py
@@ -2,7 +2,7 @@ import os
 import sys
 import argparse
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
 import pylo
 
 import commands.ruleset_export
