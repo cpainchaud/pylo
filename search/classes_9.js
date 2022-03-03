cb4fee66bb29fecf62d2c350f2d47c7b19f04bc8
cpainchaud <cpainchaud@users.noreply.github.com>
2022-03-03 10:27:40 +0000

    deploy: f58a9d426355d4ce90266c94c45633c7982a80b1

--- search/classes_9.js
+++ search/classes_9.js
@@ -5,5 +5,6 @@ var searchData=
   ['pyloapiex_2',['PyloApiEx',['../classpylo_1_1_exception_1_1_pylo_api_ex.html',1,'pylo::Exception']]],
   ['pyloapitoomanyrequestsex_3',['PyloApiTooManyRequestsEx',['../classpylo_1_1_exception_1_1_pylo_api_too_many_requests_ex.html',1,'pylo::Exception']]],
   ['pyloapiunexpectedsyntax_4',['PyloApiUnexpectedSyntax',['../classpylo_1_1_exception_1_1_pylo_api_unexpected_syntax.html',1,'pylo::Exception']]],
-  ['pyloex_5',['PyloEx',['../classpylo_1_1_exception_1_1_pylo_ex.html',1,'pylo::Exception']]]
+  ['pyloex_5',['PyloEx',['../classpylo_1_1_exception_1_1_pylo_ex.html',1,'pylo::Exception']]],
+  ['pyloobjectnotfound_6',['PyloObjectNotFound',['../classpylo_1_1_exception_1_1_pylo_object_not_found.html',1,'pylo::Exception']]]
 ];
