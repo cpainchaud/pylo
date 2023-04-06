428acc137aceb12a0387366179d11b0aacc517bd
Christophe Painchaud <shellescape@gmail.com>
2023-04-06 14:43:17 +0200

    feat(ruleset_export.py): add support for specifying output format and directory The script now supports specifying the output format and directory via the --format and --output arguments respectively. The output file name is generated using the make_filename_with_timestamp function and the output file extension is determined based on the specified format. The output file is then written to the specified directory.

--- pylo/cli/commands/ruleset_export.py
+++ pylo/cli/commands/ruleset_export.py
@@ -1,5 +1,8 @@
-import pylo
 import argparse
+import os
+from typing import Dict
+
+import pylo
 from .misc import make_filename_with_timestamp
 from . import Command
 
@@ -8,17 +11,24 @@ command_name = 'rule-export'
 
 def fill_parser(parser: argparse.ArgumentParser):
     parser.add_argument('--output', required=False, default='.')
+    parser.add_argument('--format', required=False, default='excel', choices=['csv', 'excel'])
 
 
-def __main(options, org: pylo.Organization, **kwargs):
-
-    output_file_prefix = make_filename_with_timestamp('rule_export_')
-    output_file_csv = output_file_prefix + '.csv'
-    output_file_excel = output_file_prefix + '.xlsx'
-
+def __main(options: Dict, org: pylo.Organization, **kwargs):
     csv_report_headers = ['ruleset', 'scope', 'type', 'consumers', 'providers', 'services', 'options',
                           'ruleset_href',
                           ]
+
+    output_file_format = options.get('format')
+    if output_file_format == "excel":
+        output_file_extension = ".xlsx"
+    elif output_file_format == "csv":
+        output_file_extension = ".csv"
+    else:
+        raise Exception("Unknown output file format: %s" % output_file_format)
+
+    output_file_name = options.get('output') + os.sep + make_filename_with_timestamp('rule_export_') + output_file_extension
+    output_file_name = os.path.abspath(output_file_name)
 
     csv_report = pylo.ArrayToExport(csv_report_headers)
 
@@ -45,12 +55,16 @@ def __main(options, org: pylo.Organization, **kwargs):
                 data['type'] = 'extra'
             csv_report.add_line_from_object(data)
 
-    print(" * Writing export file '{}' ... ".format(output_file_csv), end='', flush=True)
-    csv_report.write_to_csv(output_file_csv)
-    print("DONE")
-    print(" * Writing export file '{}' ... ".format(output_file_excel), end='', flush=True)
-    csv_report.write_to_excel(output_file_excel)
-    print("DONE")
+    if output_file_format == "csv":
+        print(" * Writing export file '{}' ... ".format(output_file_name), end='', flush=True)
+        csv_report.write_to_csv(output_file_name)
+        print("DONE")
+    elif output_file_format == "excel":
+        print(" * Writing export file '{}' ... ".format(output_file_name), end='', flush=True)
+        csv_report.write_to_excel(output_file_name)
+        print("DONE")
+    else:
+        raise pylo.PyloEx("Unknown format: '{}'".format(options['format']))
 
 
 command_object = Command(command_name, __main, fill_parser)
