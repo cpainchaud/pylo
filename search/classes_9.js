3c51f37318598ec7ee07e6637132fb8c87690ad3
cpainchaud <cpainchaud@users.noreply.github.com>
2023-04-21 09:21:52 +0000

    deploy: 520abcc41693aaea20cd6d2e923a269afbee3987

--- search/classes_9.js
+++ search/classes_9.js
@@ -6,9 +6,10 @@ var searchData=
   ['labelgroup_3',['LabelGroup',['../classpylo_1_1_label_group_1_1_label_group.html',1,'pylo::LabelGroup']]],
   ['labelgroupobjectjsonstructure_4',['LabelGroupObjectJsonStructure',['../classpylo_1_1_a_p_i_1_1_json_payload_types_1_1_label_group_object_json_structure.html',1,'pylo::API::JsonPayloadTypes']]],
   ['labelgroupobjectupdatejsonstructure_5',['LabelGroupObjectUpdateJsonStructure',['../classpylo_1_1_a_p_i_1_1_json_payload_types_1_1_label_group_object_update_json_structure.html',1,'pylo::API::JsonPayloadTypes']]],
-  ['labelobjectcreationjsonstructure_6',['LabelObjectCreationJsonStructure',['../classpylo_1_1_a_p_i_1_1_json_payload_types_1_1_label_object_creation_json_structure.html',1,'pylo::API::JsonPayloadTypes']]],
-  ['labelobjectjsonstructure_7',['LabelObjectJsonStructure',['../classpylo_1_1_a_p_i_1_1_json_payload_types_1_1_label_object_json_structure.html',1,'pylo::API::JsonPayloadTypes']]],
-  ['labelobjectupdatejsonstructure_8',['LabelObjectUpdateJsonStructure',['../classpylo_1_1_a_p_i_1_1_json_payload_types_1_1_label_object_update_json_structure.html',1,'pylo::API::JsonPayloadTypes']]],
-  ['labelparser_9',['LabelParser',['../classpylo_1_1cli_1_1_native_parsers_1_1_label_parser.html',1,'pylo::cli::NativeParsers']]],
-  ['labelstore_10',['LabelStore',['../classpylo_1_1_label_store_1_1_label_store.html',1,'pylo::LabelStore']]]
+  ['labelhrefref_6',['LabelHrefRef',['../classpylo_1_1_a_p_i_1_1_json_payload_types_1_1_label_href_ref.html',1,'pylo::API::JsonPayloadTypes']]],
+  ['labelobjectcreationjsonstructure_7',['LabelObjectCreationJsonStructure',['../classpylo_1_1_a_p_i_1_1_json_payload_types_1_1_label_object_creation_json_structure.html',1,'pylo::API::JsonPayloadTypes']]],
+  ['labelobjectjsonstructure_8',['LabelObjectJsonStructure',['../classpylo_1_1_a_p_i_1_1_json_payload_types_1_1_label_object_json_structure.html',1,'pylo::API::JsonPayloadTypes']]],
+  ['labelobjectupdatejsonstructure_9',['LabelObjectUpdateJsonStructure',['../classpylo_1_1_a_p_i_1_1_json_payload_types_1_1_label_object_update_json_structure.html',1,'pylo::API::JsonPayloadTypes']]],
+  ['labelparser_10',['LabelParser',['../classpylo_1_1cli_1_1_native_parsers_1_1_label_parser.html',1,'pylo::cli::NativeParsers']]],
+  ['labelstore_11',['LabelStore',['../classpylo_1_1_label_store_1_1_label_store.html',1,'pylo::LabelStore']]]
 ];
