7d9b1f522d3358b1f725d177a9adf0bca514f212
Christophe Painchaud <shellescape@gmail.com>
2026-02-09 23:55:46 +0100

    refactor(workload_export): breakdown into smaller functions to ease tests implementation

--- illumio_pylo/cli/commands/workload_export.py
+++ illumio_pylo/cli/commands/workload_export.py
@@ -1,7 +1,7 @@
 import argparse
 import sys
 from datetime import datetime
-from typing import List, Dict
+from typing import List, Dict, Optional, Any
 
 import illumio_pylo as pylo
 from illumio_pylo import ExcelHeader, ExcelHeaderSet
@@ -30,11 +30,146 @@ class ExtraColumn:
     def apply_cli_args(self, parser: argparse.ArgumentParser):
         raise NotImplementedError()
 
-    def post_process_cli_args(self, args: Dict[str, any], org: pylo.Organization):
+    def post_process_cli_args(self, args: Dict[str, Any], org: pylo.Organization):
         raise NotImplementedError()
 
 
 extra_columns: List[ExtraColumn] = []
+
+
+class ExtraColumnRegistry:
+    """Registry for managing extra columns in a testable way."""
+
+    def __init__(self):
+        self._columns: List[ExtraColumn] = []
+
+    def register(self, column: ExtraColumn) -> None:
+        """Register a new extra column."""
+        self._columns.append(column)
+
+    def clear(self) -> None:
+        """Clear all registered columns."""
+        self._columns.clear()
+
+    def get_all(self) -> List[ExtraColumn]:
+        """Get all registered columns."""
+        return self._columns.copy()
+
+    def get_column_descriptions(self) -> List[ExtraColumn.ColumnDescription]:
+        """Get column descriptions for all registered columns."""
+        return [col.column_description() for col in self._columns]
+
+
+# Global registry instance for backward compatibility
+_extra_column_registry = ExtraColumnRegistry()
+
+# Constants for filter field names
+FILTER_FIELD_HOSTNAME = 'hostname'
+FILTER_FIELD_APP = 'app'
+FILTER_FIELD_IP = 'ip'
+
+
+def format_date_or_none(date: Optional[datetime]) -> Optional[str]:
+    """Convert a date to string format, or return None if date is None."""
+    if date is None:
+        return None
+    return datetime.strftime(date, '%Y-%m-%d %H:%M:%S')
+
+
+def build_workload_row(workload: pylo.Workload, org: pylo.Organization) -> Dict[str, Any]:
+    """Build a report row dictionary from a workload object."""
+    new_row = {
+        'name': workload.forced_name,
+        'hostname': workload.hostname,
+        'href': workload.href,
+        'online': workload.online,
+        'managed': not workload.unmanaged,
+        'status': workload.get_status_string(),
+        'link_to_pce': workload.href,
+    }
+
+    for label_type in org.LabelStore.label_types:
+        new_row[f'label_{label_type}'] = workload.get_label_name(label_type)
+
+    if workload.ven_agent is not None:
+        new_row['agent.href'] = workload.ven_agent.href
+        new_row['agent.sec_policy_sync_state'] = workload.ven_agent.get_status_security_policy_sync_state()
+        new_row['agent.last_heartbeat'] = format_date_or_none(workload.ven_agent.get_last_heartbeat_date())
+        new_row['agent.sec_policy_applied_at'] = format_date_or_none(workload.ven_agent.get_status_security_policy_applied_at())
+
+    for extra_column in extra_columns:
+        new_row[extra_column.column_description().name] = extra_column.get_value(workload, org)
+
+    return new_row
+
+
+def match_filter_row_against_workload(workload: pylo.Workload, filter_data_row: Dict[str, Any], filter_fields: List[str]) -> bool:
+    """Check if a workload matches all criteria in a filter row."""
+    for filter_field_from_csv in filter_data_row:
+        if filter_field_from_csv not in filter_fields:
+            continue
+
+        current_filter = filter_data_row[filter_field_from_csv]
+        if current_filter is None:
+            continue
+
+        if filter_field_from_csv == FILTER_FIELD_HOSTNAME:
+            hostname_in_csv = pylo.hostname_from_fqdn(current_filter).lower()
+            workload_hostname = pylo.hostname_from_fqdn(workload.hostname).lower()
+            if hostname_in_csv != workload_hostname:
+                return False
+        elif filter_field_from_csv == FILTER_FIELD_APP:
+            if current_filter is None or current_filter == '':
+                continue
+            else:
+                if workload.app_label is None or workload.app_label.name.lower() != current_filter.lower():
+                    return False
+        elif filter_field_from_csv == FILTER_FIELD_IP:
+            found_ip = False
+            for interface in workload.interfaces:
+                if current_filter == interface.ip:
+                    found_ip = True
+                    break
+            if not found_ip:
+                return False
+        else:
+            # we don't support this filter type so we raise an error
+            raise ValueError(f"Filter field '{filter_field_from_csv}' is not supported")
+
+    return True
+
+
+def build_report_headers(org: pylo.Organization, include_extra_columns: bool = True) -> ExcelHeaderSet:
+    """Build the Excel header set for the workload export report."""
+    csv_report_headers = ExcelHeaderSet(['name', 'hostname'])
+    for label_type in org.LabelStore.label_types:
+        csv_report_headers.append(f'label_{label_type}')
+
+    csv_report_headers.extend([
+        'online', 'managed', 'status', 'agent.last_heartbeat',
+        'agent.sec_policy_sync_state', 'agent.sec_policy_applied_at',
+        ExcelHeader(name='link_to_pce', wrap_text=False, url_text='See in PCE', is_url=True),
+        'href', 'agent.href'])
+
+    if include_extra_columns:
+        for extra_column in extra_columns:
+            csv_report_headers.append(extra_column.column_description().name)
+
+    return csv_report_headers
+
+
+def find_matching_filters_for_workload(workload: pylo.Workload, filter_data, filter_fields: List[str]) -> List[Dict[str, Any]]:
+    """Find all filter rows that match a given workload."""
+    matching_filters = []
+
+    for filter_data_row in filter_data.objects():
+        try:
+            if match_filter_row_against_workload(workload, filter_data_row, filter_fields):
+                matching_filters.append(filter_data_row)
+        except ValueError as e:
+            raise pylo.PyloEx(str(e))
+
+    return matching_filters
 
 
 def fill_parser(parser: argparse.ArgumentParser):
@@ -53,7 +188,7 @@ def fill_parser(parser: argparse.ArgumentParser):
                         help='CSV or Excel input filename')
     parser.add_argument('--filter-file-delimiter', type=str, required=False, default=',',
                         help='CSV field delimiter')
-    parser.add_argument('--filter-fields', type=str, required=False, default=None, choices=['hostname', 'app', 'ip'], nargs="+",
+    parser.add_argument('--filter-fields', type=str, required=False, default=None, choices=[FILTER_FIELD_HOSTNAME, FILTER_FIELD_APP, FILTER_FIELD_IP], nargs="+",
                         help='Fields on which you want to filter on')
     parser.add_argument('--keep-filters-in-report', action='store_true',
                         help='If you want to keep filters information in the export file (to do a table joint for example)')
@@ -71,7 +206,9 @@ def fill_parser(parser: argparse.ArgumentParser):
         extra_column.apply_cli_args(parser)
 
 
-def __main(args, org: pylo.Organization, **kwargs):
+def __main(args: Dict[str, Any], org: pylo.Organization, logger=None, **kwargs) -> None:
+    if logger is None:
+        logger = pylo.log
 
     filter_query_string = args['filter_query']
     filter_file = args['filter_file']
@@ -81,18 +218,9 @@ def __main(args, org: pylo.Organization, **kwargs):
     verbose = args['verbose']
 
     # Create headers first, then instantiate ReportWriter to create sheet
-    csv_report_headers = ExcelHeaderSet(['name', 'hostname'])
-    for label_type in org.LabelStore.label_types:
-        csv_report_headers.append(f'label_{label_type}')
-
-    csv_report_headers.extend([
-        'online', 'managed', 'status', 'agent.last_heartbeat',
-        'agent.sec_policy_sync_state', 'agent.sec_policy_applied_at',
-        ExcelHeader(name='link_to_pce', wrap_text=False, url_text='See in PCE', is_url=True),
-        'href', 'agent.href'])
+    csv_report_headers = build_report_headers(org, include_extra_columns=True)
 
     for extra_column in extra_columns:
-        csv_report_headers.append(extra_column.column_description().name)
         print(" - adding extra column from external plugin: " + extra_column.column_description().name)
 
     report_writer = ReportWriter(headers=csv_report_headers, sheet_name='workloads', filename_prefix='workload-export', force_all_wrap_text=True, args=args)
@@ -105,10 +233,10 @@ def __main(args, org: pylo.Organization, **kwargs):
 
     if filter_file is not None:
         if filter_fields is None:
-            pylo.log.error("A filter file was provided but you didn't specify on which fields they should apply")
+            logger.error("A filter file was provided but you didn't specify on which fields they should apply")
             sys.exit(1)
         if len(filter_fields) < 1:
-            pylo.log.error("A filter file was provided but you specified an empty filter-fields option")
+            logger.error("A filter file was provided but you specified an empty filter-fields option")
             sys.exit(1)
         for field in filter_fields:
             filter_csv_expected_fields.append({'name': field, 'optional': False})
@@ -138,40 +266,14 @@ def __main(args, org: pylo.Organization, **kwargs):
             all_workloads = {w.href: w for w in matching_workloads}
             print("   - Filter query matched {} workload(s)".format(len(all_workloads)))
         except pylo.PyloEx as e:
-            pylo.log.error("Filter query error: {}".format(e))
+            logger.error("Filter query error: {}".format(e))
             sys.exit(1)
 
     used_filters = {}
 
     def add_workload_to_report(wkl: pylo.Workload = None, filter=None, filter_append_prefix='_'):
-        labels = workload.get_labels_str_list()
-
-        def none_or_date(date):
-            if date is None:
-                return None
-            return datetime.strftime(date, '%Y-%m-%d %H:%M:%S')
-
         if wkl is not None:
-            new_row = {
-                'name': wkl.forced_name,
-                'hostname': wkl.hostname,
-                'href': wkl.href,
-                'online': wkl.online,
-                'managed': not wkl.unmanaged,
-                'status': wkl.get_status_string(),
-                'link_to_pce': wkl.href,
-            }
-            for label_type in org.LabelStore.label_types:
-                new_row[f'label_{label_type}'] = wkl.get_label_name(label_type)
-
-            if wkl.ven_agent is not None:
-                new_row['agent.href'] = wkl.ven_agent.href
-                new_row['agent.sec_policy_sync_state'] = wkl.ven_agent.get_status_security_policy_sync_state()
-                new_row['agent.last_heartbeat'] = none_or_date(wkl.ven_agent.get_last_heartbeat_date())
-                new_row['agent.sec_policy_applied_at'] = none_or_date(wkl.ven_agent.get_status_security_policy_applied_at())
-
-            for extra_column in extra_columns:
-                new_row[extra_column.column_description().name] = extra_column.get_value(wkl, org)
+            new_row = build_workload_row(wkl, org)
         else:
             new_row = {}
 
@@ -188,65 +290,17 @@ def __main(args, org: pylo.Organization, **kwargs):
         if verbose:
             print("  - Processing Wkl {}|{}".format(workload.hostname, workload.href))
         if filter_data is not None:
-            matched_filters = 0
+            try:
+                matching_filters = find_matching_filters_for_workload(workload, filter_data, filter_fields)
 
-            for filter_data_row in filter_data.objects():
-                # print("   - trying filter : ")
-                # print(filter_data_row)
-                filter_all_columns_matched = True
-                for filter_field_from_csv in filter_data_row:
-                    if not filter_all_columns_matched:
-                        break
-
-                    if filter_field_from_csv not in filter_fields:
-                        continue
-
-                    # print(" field filter={} will be evaluated".format(filter_field_from_csv))
-
-                    if filter_field_from_csv not in filter_data_row:
-                        pylo.log.error("Filter field '{}' not found in CSV file".format(filter_field_from_csv))
-                        sys.exit(1)
-
-                    current_filter = filter_data_row[filter_field_from_csv]
-                    if current_filter is None:
-                        # print('    it was empty!')
-                        continue
-
-                    if filter_field_from_csv == 'hostname':
-                        hostname_in_csv = pylo.hostname_from_fqdn(current_filter).lower()
-                        workload_hostname = pylo.hostname_from_fqdn(workload.hostname).lower()
-                        # print("   : comparing filter entry {} with wkl entry {}".format(hostname_in_csv, workload_hostname))
-                        if hostname_in_csv != workload_hostname:
-                            filter_all_columns_matched = False
-                            break
-                    elif filter_field_from_csv == 'app':
-                        if current_filter is None or current_filter == '':
-                            continue
-                        else:
-                            if workload.app_label is None or workload.app_label.name.lower() != current_filter.lower():
-                                filter_all_columns_matched = False
-                                break
-                    elif filter_field_from_csv == 'ip':
-                        found_ip = False
-                        for interface in workload.interfaces:
-                            if current_filter == interface.ip:
-                                found_ip = True
-                                break
-                        if not found_ip:
-                            filter_all_columns_matched = False
-                            break
-                    else:
-                        # we don't support this filter type so we exit with an error
-                        pylo.log.error("Filter field '{}' is not supported".format(filter_field_from_csv))
-                        sys.exit(1)
-
-                if filter_all_columns_matched:
+                for filter_data_row in matching_filters:
                     add_workload_to_report(workload, filter_data_row)
-                    matched_filters += 1
 
-            if matched_filters > 0:
-                if verbose:
-                    print("  - matched {} filters".format(matched_filters))
+                if len(matching_filters) > 0 and verbose:
+                    print("  - matched {} filters".format(len(matching_filters)))
+            except pylo.PyloEx as e:
+                logger.error(str(e))
+                sys.exit(1)
 
         else:
             add_workload_to_report(workload)
